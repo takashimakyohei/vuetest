3e4effb892885ad45915fdfd9f410281dfcc162d
takashimakyohei <hagi.suzu@icloud.com>
2020-02-29 14:23:03 +0900

    TODO追加と削除は実装できた

---
+++ main.js
@@ -0,0 +1,20 @@
+let vm = new Vue({
+  el: '#app',
+  data: {
+    items: [],
+    newitemtitle: ''
+  },
+  methods: {
+    addbutton: function () {
+      this.items.push({
+        title: this.newitemtitle, ischecked: false
+      });
+      this.newitemtitle = ''
+    },
+    deletebutton:function(index){
+      if(confirm('本当に削除しますか？')){
+      this.items.splice(index,1)
+      }
+    }
+  }
+})
